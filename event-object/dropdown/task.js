eacb12e9fc9671f462bc161b6c418d65f520e07a
Oleg Khripushin <oleg19952009@yandex.ru>
2026-04-09 23:49:42 +0300

    [200~event-object

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,12 +1,17 @@
-let dropdownValue = document.querySelector('.dropdown__value'),
-	dropdownList = document.querySelector('.dropdown__list');
+const dropdowns = document.querySelectorAll('.dropdown');
 
-dropdownValue.addEventListener('click', () => {
-	dropdownList.classList.toggle('dropdown__list_active');
+dropdowns.forEach(function (dropdown) {
+	let dropdownValue = dropdown.querySelector('.dropdown__value'),
+		dropdownItems = dropdown.querySelectorAll('.dropdown__item'),
+		dropdownList = dropdown.querySelector('.dropdown__list');
+
+	dropdownValue.addEventListener('click', () => {
+		dropdownList.classList.toggle('dropdown__list_active');
+	});
+
+	dropdownItems.forEach(el => el.addEventListener('click', function (event) {
+		dropdownValue.textContent = el.textContent;
+		dropdownList.classList.toggle('dropdown__list_active');
+		event.preventDefault()
+	}));
 });
-
-document.querySelectorAll('.dropdown__item').forEach(el => el.addEventListener('click', function (event) {
-	dropdownValue.textContent = el.textContent;
-	dropdownList.classList.toggle('dropdown__list_active');
-	event.preventDefault()
-}));
--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,11 +1,15 @@
-let arrTabs = Array.from(document.querySelectorAll(".tab")),
-	arrInfo = Array.from(document.querySelectorAll(".tab__content"));
+const tabs = document.querySelectorAll('.tabs');
 
-document.querySelectorAll('.tab').forEach(el => el.addEventListener('click', function (event) {
-	let i = arrTabs.indexOf(el, 0);
-	document.querySelector(".tab__content_active").classList.remove("tab__content_active");
-	arrInfo[i].classList.toggle("tab__content_active");
-	document.querySelector(".tab_active").classList.remove("tab_active");
-	arrTabs[i].classList.toggle("tab_active");
-	event.preventDefault()
-}));
+tabs.forEach(function (tabs) {
+	let arrTabs = Array.from(tabs.querySelectorAll(".tab")),
+		arrInfo = Array.from(tabs.querySelectorAll(".tab__content"));
+
+	tabs.querySelectorAll('.tab').forEach(el => el.addEventListener('click', function (event) {
+		let i = arrTabs.indexOf(el, 0);
+		tabs.querySelector(".tab__content_active").classList.remove("tab__content_active");
+		arrInfo[i].classList.toggle("tab__content_active");
+		tabs.querySelector(".tab_active").classList.remove("tab_active");
+		arrTabs[i].classList.toggle("tab_active");
+		event.preventDefault()
+	}));
+});
